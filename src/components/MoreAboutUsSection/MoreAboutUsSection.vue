e37680e656e5b55e7bb1cb3463230afd16e56a4d
Egor Mikheev <mikheev.egor@gmail.com>
2021-03-17 14:37:53 +0300

    feat unfederalreserve/general#160 9

--- src/components/MoreAboutUsSection/MoreAboutUsSection.vue
+++ src/components/MoreAboutUsSection/MoreAboutUsSection.vue
@@ -2,7 +2,7 @@
   section.MoreAboutUsSection.more-about-us
     h2 About Us
     p Since its founding, UnFederalreserve.com has been focused on transparency,
-      |  efficiency, and professionalism. Our goal is to exceed clients' expectations
+      |  efficiency, and professionalism. Our goal is to exceed clients expectations
       |  as they enjoy seamless lending and borrowing experiences. Please reach out
       |  to learn more about the UnFederalReserve management team, the company, and
       |  the additional services we provide.
