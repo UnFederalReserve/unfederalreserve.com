81741988440516f2c0eda9add2cba0585017d5d5
Nikolay Krasikov <masterflomastermaster@gmail.com>
2021-06-30 16:44:52 +0300

    upd text (#105)

--- src/components/RoadmapSection/RoadmapSection.vue
+++ src/components/RoadmapSection/RoadmapSection.vue
@@ -20,23 +20,39 @@
         ul
           li ReserveLending® Alpha testing and product launch, with single-side staking
           li Voting on Tier 1 exchange listing and key relationships
+          li Tier 1 Exchange Listing
+          li Complete organizational chart
+          li Gather business requirements for ReserveFunding
+          li Chainlink® price oracle integration for all non eRSDL tokens.
     .roadmap-row
       .roadmap-item
         h4 Q3 2021
         ul
           li MVP with Partner non-bank lenders
-          li Voting on CeFi participants’ size limits, if needed
-          li Apply for multi-jurisdictional regulatory licenses to open cross-border opportunities
-          li Launch Lightning product (low rate, unsecured credit) as safe harbor for commercial lending
+          li Apply for multi-jurisdictional regulatory licenses
+          li Use DeFI platform to fund CeFI entities
+          li Creation of a KYC/AML only fork of ReserveLending© (new ReserveFunding product) with perks for volunteering to get KYC'd.
+          li Implement strategies for putting excess supply to work in DeFi and a never been done CeFi (USA regulatory compliant) yield strategy.
+          li Launch ReserveFunding (Lightning® and Thunder®)
+          li Manually process cash flows and replace them with pipes
+          li Start repurchasing eRSDL as ReserveFunding non-bank lenders start shadow licensing our platform to access liquidity
     .roadmap-row
       .roadmap-item
         h4 Q4 2021
         ul
-          li Fully decentralized platform providing capital to lenders in need, and providing returns for folks with excess capital
           li Asset class discussion on break out of more discrete lender asset-types to refine pricing
-          li Secure insurance for banking product
+          li Secure insurance for a banking product
           li CMDash product or integration with Treasury tool provider
-          li Institute sponsor-bank model for expanding user base
+          li ReserveFunding 80% automated volume at $200mn daily
+          li Start retiring eRSDL / long-term lock / other @ 1% total borrow avg outstanding balance
+          li Combination CeFI and DeFI funding CeFI
+    .roadmap-row
+      .roadmap-item
+        h4 Q1 2022
+        ul
+          li Fully decentralized platform providing capital to lenders in need and providing returns for folks with excess capital
+          li Launch ReserveB2B
+          li Add another T1 exchange
 </template>
 
 <script>
@@ -55,14 +71,14 @@ export default {
   max-width: 1010px
   padding-left: 15px
   padding-right: 15px
-  height: 2000px
+  height: 2830px
   margin: 0 auto
   margin-top: 200px
   background-image: url('~images/roadmap.svg')
   background-repeat: no-repeat
   background-position: center 100px
   background-size: 100%
-  @media screen and (max-width: 767px)
+  @media screen and (max-width: 991px)
     background-image: none
     margin-top: 100px
     height: auto
@@ -72,7 +88,9 @@ export default {
   &-item
     width: 100%
     max-width: 380px
-    @media screen and (max-width: 767px)
+    li
+      margin-bottom: 15px
+    @media screen and (max-width: 991px)
       max-width: 100%
     *
       color: $white
@@ -81,43 +99,58 @@ export default {
   &-row
     display: flex
     margin-bottom: 50px
-    @media screen and (max-width: 767px)
+    @media screen and (max-width: 991px)
       margin-bottom: 20px
     &:nth-child(2)
       .roadmap-item
         margin-left: 100px
-        margin-top: 220px
-        @media screen and (max-width: 767px)
+        margin-top: 280px
+        @media screen and (max-width: 991px)
           margin-left: 0
           margin-top: 50px
     &:nth-child(3)
       justify-content: flex-end
       .roadmap-item
         margin-right: 50px
-        margin-top: 0px
-        @media screen and (max-width: 767px)
+        margin-top: 50px
+        @media screen and (max-width: 991px)
           margin-right: 0
+          margin-top: 0
     &:nth-child(4)
       .roadmap-item
         margin-left: 50px
-        margin-top: 0px
-        @media screen and (max-width: 767px)
+        margin-top: 30px
+        @media screen and (max-width: 991px)
           margin-left: 0
+          margin-top: 0
     &:nth-child(5)
       margin-bottom: 0
       justify-content: flex-end
-      @media screen and (max-width: 767px)
+      @media screen and (max-width: 991px)
         margin-bottom: 20px
       .roadmap-item
         margin-right: 20px
-        margin-top: 20px
-        @media screen and (max-width: 767px)
+        margin-top: -180px
+        @media screen and (max-width: 991px)
           margin-right: 0
+          margin-top: 0
     &:nth-child(6)
       margin-bottom: 0
+      @media screen and (max-width: 991px)
+        margin-bottom: 20px
       .roadmap-item
         margin-left: 50px
-        margin-top: 0px
-        @media screen and (max-width: 767px)
+        margin-top: -80px
+        @media screen and (max-width: 991px)
           margin-left: 0
+          margin-top: 0
+    &:nth-child(7)
+      margin-bottom: 0
+      justify-content: flex-end
+      .roadmap-item
+        margin-right: 20px
+        margin-top: -180px
+        @media screen and (max-width: 991px)
+          margin-left: 0
+          margin-top: 0
 </style>
