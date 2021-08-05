3dd2b627504fbde40d57315e122ea15ae6392f3b
ColtMoney <masterflomastermaster@gmail.com>
2021-08-05 15:21:25 +0300

    feat/924 upd style for header https://github.com/UnFederalReserve/development/issues/924

--- src/App.vue
+++ src/App.vue
@@ -78,5 +78,5 @@ export default {
 @import 'styles/app.scss'
 body,
 .v-application
-  background: linear-gradient(180deg, #2F53CB 0%, #2F53CB 10%, #010450 100%) !important
+  background: linear-gradient(180deg, #345BD8 0%, #010450 100%) !important
 </style>
