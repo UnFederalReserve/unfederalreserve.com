7aec10fcd3f574406a4bacf88258d815c66b5611
Mark Watson <mkw5053@gmail.com>
2021-09-24 09:08:39 -0700

    Added new lines to privacy policy

--- src/pages/PagePrivacyPolicy.vue
+++ src/pages/PagePrivacyPolicy.vue
@@ -3,22 +3,40 @@
     h2
       strong Residual Token Inc. Privacy Policy
     p
-      | Residual Token Inc. (&quot;Residual Token&quot;, &ldquo;We&rdquo; or &ldquo;Our&rdquo;) is committed to protecting the privacy of users of its software applications and services. This Privacy Policy states the Residual Token practices applying to individual users of the Residual Token managed services (the &quot;Services&quot;). Residual Token owns, operates, and maintains the Services. Residual Token is a Delaware corporation with its principal office in New York, NY.
+      | Residual Token Inc. (&quot;Residual Token&quot;, &ldquo;We&rdquo;
+      | or &ldquo;Our&rdquo;) is committed to protecting the privacy of
+      | users of its software applications and services. This Privacy Policy
+      | states the Residual Token practices applying to individual users of
+      | the Residual Token managed services (the &quot;Services&quot;). Residual
+      | Token owns, operates, and maintains the Services. Residual Token is a Delaware
+      | corporation with its principal office in New York, NY.
     p
-      | BY ACCESSING AND USING THE SERVICES, YOU AGREE TO THE INFORMATION USE AND COLLECTION TERMS OF THIS PRIVACY POLICY RESIDUAL TOKEN HAS THE RIGHT TO REVISE THIS POLICY AT ANY TIME. WE MAY NOT NOTIFY USERS INDIVIDUALLY IF WE CHANGE THIS PRIVACY POLICY. ANY CHANGES WILL BE EFFECTIVE WHEN POSTED. WE ENCOURAGE YOU TO CHECK THIS PRIVACY POLICY FREQUENTLY FOR CHANGES.
+      | BY ACCESSING AND USING THE SERVICES, YOU AGREE TO THE INFORMATION USE AND
+      | COLLECTION TERMS OF THIS PRIVACY POLICY RESIDUAL TOKEN HAS THE RIGHT TO
+      | REVISE THIS POLICY AT ANY TIME. WE MAY NOT NOTIFY USERS INDIVIDUALLY IF WE
+      | CHANGE THIS PRIVACY POLICY. ANY CHANGES WILL BE EFFECTIVE WHEN POSTED. WE
+      | ENCOURAGE YOU TO CHECK THIS PRIVACY POLICY FREQUENTLY FOR CHANGES.
     h2
       strong Collecting Personal Information
     p
-      | When you visit our Website, we collect certain information about your device, your interaction with our Website, and information necessary to process your purchases. We may also collect additional information as part of an opt-in survey for research. In this Privacy Policy, we refer to any information that can uniquely identify an individual (including the information below) as &ldquo;Personal Information&rdquo;. See the list below for more information about what Personal Information we collect and why.
+      | When you visit our Website, we collect certain information about your device,
+      | your interaction with our Website, and information necessary to process your
+      | purchases. We may also collect additional information as part of an opt-in
+      | survey for research. In this Privacy Policy, we refer to any information that
+      | can uniquely identify an individual (including the information below) as
+      | &ldquo;Personal Information&rdquo;. See the list below for more information
+      | about what Personal Information we collect and why.
     p
       span(style='text-decoration:underline;') Device information
     ul
       li
         strong Examples of Personal Information collected:
-        |  version of web browser, IP address, time zone, cookie information, and how you interact with our Website.
+        |  version of web browser, IP address, time zone, cookie information, and how
+        | you interact with our Website.
       li
         strong Purpose of collection:
-        |  to load our Website accurately for you, to perform analytics on our Website usage, to optimize our Website, and to perform research.
+        |  to load our Website accurately for you, to perform analytics on our Website
+        | usage, to optimize our Website, and to perform research.
       li
         strong Source of collection:
         |  Collected automatically when you access our Website using cookies
@@ -41,29 +59,39 @@
       strong Minors
     p
     p
-      | Our Website is not intended for individuals under the age of 18. We do not intentionally collect Personal Information from children. If you are the parent or guardian and believe your child has provided us with Personal Information, please contact us at the address below to request deletion.
+      | Our Website is not intended for individuals under the age of 18. We do not
+      | intentionally collect Personal Information from children. If you are the
+      | parent or guardian and believe your child has provided us with Personal
+      | Information, please contact us at the address below to request deletion.
     p
     h2
       strong Sharing Personal Information
     p
-      | We share your Personal Information with service providers to help us provide our services and fulfill our contracts with you, as described above. For example:
+      | We share your Personal Information with service providers to help us provide
+      | our services and fulfill our contracts with you, as described above. For example:
     ul
       li
-        | We may share your Personal Information to comply with applicable laws and regulations, to respond to a subpoena, search warrant or other lawful request for information we receive, or to otherwise protect our rights.
+        | We may share your Personal Information to comply with applicable laws and
+        | regulations, to respond to a subpoena, search warrant or other lawful request
+        | for information we receive, or to otherwise protect our rights.
       li
-        | We share your personal information with Google Analytics to better understand how visitors use our website.
+        | We share your personal information with Google Analytics to better understand
+        | how visitors use our website.
     h2
       strong
         em Using Personal Information
     p
       em
-        | We use your personal Information to provide our services to you, which includes: offering products and keeping you up to date on new products, services, and offers.
+        | We use your personal Information to provide our services to you, which includes:
+        | offering products and keeping you up to date on new products, services, and offers.
     h3
       strong
         em Lawful basis
     p
       em
-        | Pursuant to the General Data Protection Regulation (&ldquo;GDPR&rdquo;), if you are a resident of the European Economic Area (&ldquo;EEA&rdquo;), we process your personal information under the following lawful bases:
+        | Pursuant to the General Data Protection Regulation (&ldquo;GDPR&rdquo;), if
+        | you are a resident of the European Economic Area (&ldquo;EEA&rdquo;), we
+        | process your personal information under the following lawful bases:
     ul
       li
         em Your consent;
@@ -75,22 +103,30 @@
         em To perform a task carried out in the public interest;
       li
         em
-          | For our legitimate interests, which do not override your fundamental rights and freedoms.
+          | For our legitimate interests, which do not override your fundamental rights
+          | and freedoms.
     h3
       strong
         em Retention
     p
       em
-        | When you place an order through our Website, we will retain your Personal Information for our records unless and until you ask us to erase this information. For more information on your right of erasure, please see the &lsquo;Your rights&rsquo; section below.
+        | When you place an order through our Website, we will retain your Personal
+        | Information for our records unless and until you ask us to erase this information.
+        | For more information on your right of erasure, please see the &lsquo;Your
+        | rights&rsquo; section below.
     h3
       strong
         em Automatic decision-making
     p
       em
-        | If you are a resident of the EEA, you have the right to object to processing based solely on automated decision-making (which includes profiling), when that decision-making has a legal effect on you or otherwise significantly affects you.
+        | If you are a resident of the EEA, you have the right to object to processing
+        | based solely on automated decision-making (which includes profiling), when
+        | that decision-making has a legal effect on you or otherwise significantly
+        | affects you.
     p
       em
-        | We DO NOT engage in fully automated decision-making that has a legal or otherwise significant effect using customer data.
+        | We DO NOT engage in fully automated decision-making that has a legal or otherwise
+        | significant effect using customer data.
     h2
       strong
         em Your rights
@@ -99,53 +135,91 @@
         em GDPR
     p
       em
-        | If you are a resident of the EEA, you have the right to access the Personal Information we hold about you, to port it to a new service, and to ask that your Personal Information be corrected, updated, or erased. If you would like to exercise these rights, please contact us through the contact information below.
+        | If you are a resident of the EEA, you have the right to access the Personal
+        | Information we hold about you, to port it to a new service, and to ask that
+        | your Personal Information be corrected, updated, or erased. If you would like
+        | to exercise these rights, please contact us through the contact information below.
     h3
       strong
         em CCPA
     p
       em
-        | If you are a resident of California, you have the right to access the Personal Information we hold about you (also known as the &lsquo;Right to Know&rsquo;), to port it to a new service, and to ask that your Personal Information be corrected, updated, or erased. If you would like to exercise these rights, please contact us through the contact information below.
+        | If you are a resident of California, you have the right to access the Personal
+        | Information we hold about you (also known as the &lsquo;Right to Know&rsquo;),
+        | to port it to a new service, and to ask that your Personal Information be
+        | corrected, updated, or erased. If you would like to exercise these rights,
+        | please contact us through the contact information below.
     p
       em
-        | If you would like to designate an authorized agent to submit these requests on your behalf, please contact us at the address below.
+        | If you would like to designate an authorized agent to submit these requests on
+        | your behalf, please contact us at the address below.
     h2
       strong
         em Cookies
     p
       em
-        | A cookie is a small amount of information that&rsquo;s downloaded to your computer or device when you visit our Website. We use a number of different cookies, including functional, performance, advertising, and social media or content cookies. Cookies make your browsing experience better by allowing the website to remember your actions and preferences (such as login and region selection). This means you don&rsquo;t have to re-enter this information each time you return to our Website or browse from one page to another. Cookies also provide information on how people use the website, for instance whether it&rsquo;s their first time visiting or if they are a frequent visitor.
+        | A cookie is a small amount of information that&rsquo;s downloaded to your
+        | computer or device when you visit our Website. We use a number of different
+        | cookies, including functional, performance, advertising, and social media or
+        | content cookies. Cookies make your browsing experience better by allowing the
+        | website to remember your actions and preferences (such as login and region
+        | selection). This means you don&rsquo;t have to re-enter this information each
+        | time you return to our Website or browse from one page to another. Cookies
+        | also provide information on how people use the website, for instance whether
+        | it&rsquo;s their first time visiting or if they are a frequent visitor.
     p
       em
-        | The length of time that a cookie remains on your computer or mobile device depends on whether it is a &ldquo;persistent&rdquo; or &ldquo;session&rdquo; cookie. Session cookies last until you stop browsing and persistent cookies last until they expire or are deleted. Most of the cookies we use are persistent and will expire between 30 minutes and two years from the date they are downloaded to your device.
+        | The length of time that a cookie remains on your computer or mobile device
+        | depends on whether it is a &ldquo;persistent&rdquo; or &ldquo;session&rdquo;
+        | cookie. Session cookies last until you stop browsing and persistent cookies
+        | last until they expire or are deleted. Most of the cookies we use are persistent
+        | and will expire between 30 minutes and two years from the date they are downloaded
+        | to your device.
     p
       em
-        | You can control and manage cookies in various ways. Please keep in mind that removing or blocking cookies can negatively impact your user experience and parts of our website may no longer be fully accessible.
+        | You can control and manage cookies in various ways. Please keep in mind that
+        | removing or blocking cookies can negatively impact your user experience and
+        | parts of our website may no longer be fully accessible.
     p
       em
-        | Most browsers automatically accept cookies, but you can choose whether or not to accept cookies through your browser controls, often found in your browser&rsquo;s &ldquo;Tools&rdquo; or &ldquo;Preferences&rdquo; menu. For more information on how to modify your browser settings or how to block, manage or filter cookies can be found in your browser&rsquo;s help file or through such sites as www.allaboutcookies.org
+        | Most browsers automatically accept cookies, but you can choose whether or not
+        | to accept cookies through your browser controls, often found in your
+        | browser&rsquo;s &ldquo;Tools&rdquo; or &ldquo;Preferences&rdquo; menu. For more
+        | information on how to modify your browser settings or how to block, manage or
+        | filter cookies can be found in your browser&rsquo;s help file or through such
+        | sites as www.allaboutcookies.org
         | .
     p
       em
-        | Additionally, please note that blocking cookies may not completely prevent how we share information with third parties such as our advertising partners. To exercise your rights or opt-out of certain uses of your information by these parties, please follow the instructions in the &ldquo;Behavioural Advertising&rdquo; section above.
+        | Additionally, please note that blocking cookies may not completely prevent how
+        | we share information with third parties such as our advertising partners. To
+        | exercise your rights or opt-out of certain uses of your information by these
+        | parties, please follow the instructions in the &ldquo;Behavioural
+        | Advertising&rdquo; section above.
     h3
       strong
         em Do Not Track
     p
       em
-        | Please note that because there is no consistent industry understanding of how to respond to &ldquo;Do Not Track&rdquo; signals, we do not alter our data collection and usage practices when we detect such a signal from your browser.
+        | Please note that because there is no consistent industry understanding of how
+        | to respond to &ldquo;Do Not Track&rdquo; signals, we do not alter our data
+        | collection and usage practices when we detect such a signal from your browser.
     h2
       strong
         em Changes
     p
       em
-        | We may update this Privacy Policy from time to time in order to reflect, for example, changes to our practices or for other operational, legal, or regulatory reasons.
+        | We may update this Privacy Policy from time to time in order to reflect, for
+        | example, changes to our practices or for other operational, legal, or regulatory
+        | reasons.
     h2
       strong
         em Contact
     p
       em
-        | For more information about our privacy practices, if you have questions, or if you would like to make a complaint, please contact us by e-mail at info@unfederalreserve.com
+        | For more information about our privacy practices, if you have questions, or if
+        | you would like to make a complaint, please contact us by e-mail at
+        | info@unfederalreserve.com
         |  or by mail using the details provided below:
     p
       em 228 Park Ave S, PMB 96932, New York, New York 10003-1502
@@ -154,9 +228,8 @@
 </template>
 
 <script>
-
 export default {
-  name: 'PageTerms',
+  name: "PageTerms"
 };
 </script>
 
