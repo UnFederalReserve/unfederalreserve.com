c6162a134e4c43589df8effa9ab23fe0bb1e3cb7
ColtMoney <masterflomastermaster@gmail.com>
2022-07-19 15:00:36 +0300

    replace forum link to Buy eRSDL

--- src/components/TheFooter/TheFooter.vue
+++ src/components/TheFooter/TheFooter.vue
@@ -63,9 +63,9 @@
               target="_blank",
             ) LP Staking
             a(
-              href="https://forum.unfederalreserve.com",
+              href="https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x5218e472cfcfe0b64a064f055b43b4cdc9efd3a6&chain=mainnet",
               target="_blank",
-            ) Forum
+            ) Buy eRSDL
         .footer-content-row.between
           .footer-content-row__contacts.column
               h5 We Want to Hear from You
